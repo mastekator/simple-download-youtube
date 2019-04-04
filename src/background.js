816bbfce45c06bf351b8431be2c0c4072ae255ad
Maxim <akmiskam@yandex.ru>
2019-04-04 15:07:18 +0300

    gdfg

--- src/background.js
+++
@@ -1,8 +0,0 @@
-
-
-chrome.runtime.onMessage.addListener(function (request, sender, callback) {
-    console.log("received", request, sender, callback);
-
-    chrome.downloads.download({url: request.url, filename: request.name.replace("?", "")});
-
-});
